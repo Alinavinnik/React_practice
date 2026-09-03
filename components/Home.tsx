04b21b12ff2e9b6d8fafe89454ef1927e70caa80
Alina <alinavinnikhgfff@gmail.com>
2026-09-03 21:47:46 +0300

    add main page

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,5 +1,35 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="flex min-h-[calc(100vh-80px)] items-center justify-between gap-12">
+      <div className="max-w-xl -translate-y-20 pl-4">
+        <p className="mb-4 font-medium text-text-accent">
+          React Practice Playground
+        </p>
+
+        <h1 className="mb-6 text-6xl font-bold leading-tight text-text-primary">
+          Practice. Code.
+          <span className="block text-text-accent">Get better.</span>
+        </h1>
+
+        <p className="max-w-lg text-lg leading-8 text-text-secondary">
+          A personal playground for building React features from scratch,
+          practicing frontend logic, and becoming a more confident developer.
+        </p>
+      </div>
+
+      <div
+        className="
+      h-125
+      w-full
+      max-w-3xl
+      bg-(image:--hero-img)
+      bg-contain
+      bg-center
+      bg-no-repeat
+    "
+      />
+    </section>
+  );
 };
 
 export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import RootLayout from "../components/RootLayout.tsx";
+import Home from "../components/Home.tsx";
 import Accordion from "../components/Accordion/Accordion.tsx";
 
 createRoot(document.getElementById("root")!).render(
@@ -10,6 +11,7 @@ createRoot(document.getElementById("root")!).render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<RootLayout />}>
+          <Route index element={<Home />} />
           <Route path="accordion" element={<Accordion />} />
         </Route>
       </Routes>
